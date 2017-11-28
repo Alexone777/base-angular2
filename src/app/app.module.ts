dfbe94ac208e83434710778f51d3508d057cb801
a-kachan <aliakach@tele2.com>
2017-11-28 16:13:50 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/header/logo/logo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/header/menu/menu.component';
+import { LogInComponent } from './components/header/log-in/log-in.component';
+import { CourseComponent } from './components/course/course.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent,
+    FooterComponent,
+    LogInComponent,
+    CourseComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
